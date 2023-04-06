38164e79057a3831f9c4bcf159cf3e1e36915d96
SirC00kie <kostomarov.2001@mail.ru>
2023-04-06 19:26:09 +0300

    refactor solution handler

--- src/kinetics/internal/urls.py
+++ src/kinetics/internal/urls.py
@@ -3,18 +3,14 @@ from rest_framework import routers
 
 from kinetics.internal.transport.rest.handlers.input_data_handler import get_input_data, create_input_data
 from kinetics.internal.transport.rest.handlers.table_parameters_handler import get_table_parameters, create_table_parameters
-from kinetics.internal.transport.rest.handlers.solution_data_handler import SolutionDataView
+from kinetics.internal.transport.rest.handlers.solution_data_handler import get_solution
 
 app_name = 'kinetics'
-# router = routers.DefaultRouter()
-# router.register(r'inputdata', InputDataViewSet)
-# router.register(r'solutiondata', SolutionDataViewSet)
 
 urlpatterns = [
     path('tableparameters/<int:index>/', get_table_parameters),
     path('tableparameters/', create_table_parameters),
-    path('solution/<int:index>', SolutionDataView.as_view()),
+    path('solutiondata/<int:index>/', get_solution),
     path('inputdata/<int:index>/', get_input_data),
     path('inputdata/', create_input_data)
-    # path('', include(router.urls)),
 ]
